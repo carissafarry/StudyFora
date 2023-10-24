cf427619114b7bad00ca15b62ec01a91f090f718
Carissa Farry <69377985+carissafarry@users.noreply.github.com>
2023-10-24 08:40:35 +0700

    Initial Commit

---
+++ StudyFora/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  StudyFora
+//
+//  Created by Carissa Farry Hilmi Az Zahra on 24/10/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ StudyFora/StudyForaApp.swift
@@ -0,0 +1,17 @@
+//
+//  StudyForaApp.swift
+//  StudyFora
+//
+//  Created by Carissa Farry Hilmi Az Zahra on 24/10/23.
+//
+
+import SwiftUI
+
+@main
+struct StudyForaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
